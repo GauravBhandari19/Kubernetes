174068aa5c8b97c8c9ab77f62f150873e8a526b1
GauravBhandari19 <gaurav.bhandari85@gmail.com>
2020-07-10 03:28:18 +0100

    1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 // To install Express: npm install express express --save
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World, this is a just a test server with NO info!');
 });
 
 app.listen(3000, function () {
